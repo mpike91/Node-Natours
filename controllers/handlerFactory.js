a63dc15472fe129f4b66e72fc48616164654cdc2
Matt Pike <mattp91@gmail.com>
2023-07-19 15:50:20 -0400

    Began calculation of average ratings for tour reviews

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -76,6 +76,7 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
+    // const doc = await features.query.explain(); // This gives extra technical data in the response field on a request.
     const doc = await features.query;
 
     // SEND RESPONSE
