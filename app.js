540eb7e22b90a7ab6f2c064ccddd9963759039d8
Matt Pike <mattp91@gmail.com>
2023-07-21 10:02:22 -0400

    Created extender pug files

--- app.js
+++ app.js
@@ -72,8 +72,21 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.get('/', (req, res) => {
   res.status(200).render('base', {
+    title: '',
     tour: 'The Forest Hiker',
     user: 'Jonas',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: '| All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: '| The Forest Hiker Tour',
   });
 });
 
