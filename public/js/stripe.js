b42ebc3896935793a0dd498a5ab7fdbc096753ab
Matt Pike <mattp91@gmail.com>
2023-07-26 16:40:39 -0400

    Created booking model

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const bookTour = async (tourId) => {
+  const stripe = Stripe(
+    'pk_test_51NYBCsL1a25Qa2mH6dy2su7y1psocWm1IycIyuagjIOuvNj0cpph1nAz9sqnNj7HNigoGe5MhncUpLeg2rRPanKh00LiY9ITZ2'
+  );
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
