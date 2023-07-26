2112478850e12ba74de8df6fd4e66b638999489c
Matt Pike <mattp91@gmail.com>
2023-07-26 09:44:01 -0400

    Began advanced email setup

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12419,7 +12419,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38131" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34719" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
