9c690ba0b8e51fa062495bdba2a7d1f830d05e82
Matt Pike <Mattp91@gmail.com>
2023-07-28 09:52:27 -0400

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,10 +8,8 @@ export const bookTour = async (tourId) => {
   );
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
